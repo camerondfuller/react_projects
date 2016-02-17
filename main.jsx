d6018920fbb21ffde0df072cc45caa719237eae9
Cameron Fuller <franksjacket@yahoo.com>
2016-02-17 14:45:49 -0800

    Add user input bar and todo count.

--- main.jsx
+++ main.jsx
@@ -5,12 +5,7 @@ var TodoList = React.createClass({
 
    getInitialState: function(){
       return{
-         todos:[
-            {
-               title: 'placeholder todo',
-               complete: false
-            }
-         ]
+         todos:[]
       }
    },
 
@@ -23,22 +18,41 @@ var TodoList = React.createClass({
       });
       this.setState({todos:todos});
    },
+   // to get the input value, specify a ref on the input and then call it with this.refs
+   addTodo: function(event) {
+
+      event.preventDefault();
+
+      if(this.refs.addTodo.value){
+         this.state.todos.push({title:this.refs.addTodo.value, complete:false});
+         this.setState({todos: this.state.todos});
+         this.refs.addTodo.value = '';
+      }
+   },
 
    renderTodos: function(todo, index){
-      return   <Todo key={index}
-                     id={index}
-                     toggleComplete={this.toggleComplete}
-                     item={todo}/>;
+      return  (<Todo key={index} id={index} toggleComplete={this.toggleComplete} item={todo}/>);
    },
 
    render: function() {
+      var number = this.state.todos.length;
       return (
          <div className='todo-list'>
             <h1>Todo List!</h1>
+            <div className="add-todo">
+               <form name="addTodoForm" onSubmit={this.addTodo}>
+                  <input type="text" ref="addTodo"/>
+               </form>
+            </div>
             <ul>
                {this.state.todos.map(this.renderTodos)}
                {/*the map method works just like the forEach and the $.each method; they loop over an array*/}
             </ul>
+            <div className="todo-admin">
+               <div>
+                  {number} {number > 1 || number === 0 ? "todos" : "todo"}
+               </div>
+            </div>
          </div>
       )
    }

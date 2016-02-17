669fb59b703b76feeed77a12ca0347a6795bc1fe
Cameron Fuller <franksjacket@yahoo.com>
2016-02-17 10:50:39 -0800

    Setup beginning state for todo app.

--- main.jsx
+++ main.jsx
@@ -1,9 +1,24 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var TodoList = React.createClass({
+
+   getInitialState: function(){
+      return{
+         todos:[]
+      }
+   },
+
+   render: function() {
+      return (
+         <div className='todo-list'>
+            <p>Hello from React!</p>
+         </div>
+      )
+   }
+});
 
 
 
 
-
-ReactDOM.render(< />, document.querySelector('.tweet-box'));
+ReactDOM.render(<TodoList />, document.querySelector('#todo-app'));
